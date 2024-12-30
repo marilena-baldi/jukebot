6129b12344aa21dd92f608600555f9b6b13e1d2c
marilena-baldi <marilenabaldi95@gmail.com>
2024-12-30 19:19:29 +0100

    removed dotenv

--- src/bot/lib/__init__.py
+++ src/bot/lib/__init__.py
@@ -1,13 +1,10 @@
 import os
 import logging
 from kink import di
-from dotenv import load_dotenv
 from .cogs.admin import Admin
 from .cogs.music import Music
 from .bot import Bot
 from .youtube import Youtube
-
-load_dotenv()
 
 logging.getLogger().setLevel(os.environ.get('LOG_LEVEL', 'INFO'))
 
